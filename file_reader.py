332d3e5aae7764d608ad4bbdb5ba46b3f6b948fe
Zuhair <84449586+MohammedZuhairAhmed@users.noreply.github.com>
2023-12-31 11:58:12 +0530

    Delete file_reader.py

--- file_reader.py
+++
@@ -1,9 +0,0 @@
-file_path = input("Enter the path of the file: ")
-
-try:
-    with open(file_path, 'r') as file:
-        content = file.read()
-        print("File content:")
-        print(content)
-except FileNotFoundError:
-    print("File not found. Please check the file path.")
